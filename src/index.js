f8161ccabad281d0641cf592c21cb5bb926ba107
GloriaAsaso <adwoaoglory@gmail.com>
2024-12-27 12:52:21 +0000

    adding all files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+
+let name = prompt("what is your name?");
+
+let message = prompt("please enter your message");
+
+let email = prompt("type your email here");
+
+alert("Hello $ {name}, Thank you for your ${message} and we will serve you soon via your ${email}🌹👍");
